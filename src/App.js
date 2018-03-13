205c18bbfe969e7f8b8a612093cefadf5937162a
lulu27753 <lulu27753@163.com>
2018-03-13 22:16:08 +0800

    react-redux链接store

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { add, remove, addAsync } from './index.redux';
+
 class App extends React.Component {
     render() {
-      const store = this.props.store;
-      const num = store.getState();
+      const num = this.props.num;
       const add = this.props.add;
       const remove = this.props.remove;
       const addAsync = this.props.addAsync;
       return (
         <div>
           <h1>现在{num}</h1>
-          <button onClick={() => store.dispatch(add())}>增加</button>
-          <button onClick={() => store.dispatch(remove())}>减少</button>
-          <button onClick={() => store.dispatch(addAsync())}>拖两天再给</button>
+          <button onClick={() => add()}>增加</button>
+          <button onClick={() => remove()}>减少</button>
+          <button onClick={() => addAsync()}>拖两天再给</button>
         </div>
       )
     }
 }
+// 从store里面提取你需要的数据
+const mapStatetoProp = (state) => {
+  return { num : state }
+}
+const actionCreators = { add, remove, addAsync };
+// connect()会把store中的state和action映射到props中
+App = connect(mapStatetoProp, actionCreators)(App);
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
-import App from './App';
-import { counter, add, remove, addAsync } from './index.redux';
 import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
+import App from './App';
+import { counter } from './index.redux';
+
 
 const reduxDevtools = window.devToolsExtension ? window.devToolsExtension() : f => f;
 const store = createStore(counter, compose(
   applyMiddleware(thunk),
   reduxDevtools
 ));
-function render() {
-  ReactDom.render(<App store={store} add={add} remove={remove} addAsync={addAsync}/>, document.getElementById('root'))
-}
-render();
 
-store.subscribe(render);
+ReactDom.render(
+  (<Provider store={store}>
+    <App />
+  </Provider>)
+  , document.getElementById('root'))
