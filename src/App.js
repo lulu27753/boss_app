894e9cd5bae0212fc6b23a59ff703637dabc9100
lulu27753 <lulu27753@163.com>
2018-03-13 15:50:27 +0800

    优化解耦

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { add } from './index.redux';
 class App extends React.Component {
     render() {
       const store = this.props.store;
-      const num = store.getState()
+      const num = store.getState();
+      const add = this.props.add;
+      const remove = this.props.remove;
       return (
         <div>
           <h1>现在{num}</h1>
           <button onClick={() => store.dispatch(add())}>增加</button>
-          <button>减少</button>
+          <button onClick={() => store.dispatch(remove())}>减少</button>
         </div>
       )
     }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { createStore } from 'redux';
 import App from './App';
-import { counter } from './index.redux';
+import { counter, add, remove } from './index.redux';
 
 const store = createStore(counter);
 function render() {
-  ReactDom.render(<App store={store} />, document.getElementById('root'))
+  ReactDom.render(<App store={store} add={add} remove={remove}/>, document.getElementById('root'))
 }
 render();
 
