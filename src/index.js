a02d1270ebff0ec541fa1f2f08c7f129eb651b0d
lulu27753 <lulu27753@163.com>
2018-03-13 11:15:59 +0800

    redux 原理

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+// reducer根据老的state和action。生成新的state
+function counter(state=0, action) {
+  switch (action.type) {
+    case 'increase':
+      return state + 1;
+    case 'decrease':
+      return state - 1;
+    default:
+      return 10
+  }
+}
+// 新建store,将reducer传入进去
+const store = createStore(counter)
+// 可以通过getState()获取store的状态
+const init = store.getState()
+console.log(init);
+
+// 定义一个监听事件，获取当前store的状态
+function listener() {
+  const current = store.getState()
+  console.log(current);
+}
+// 订阅一下该事件
+store.subscribe(listener);
+// 通过dispatch派发事件来更改store的状态,传递action对象
+store.dispatch({type: 'increase'})
+store.dispatch({type: 'increase'})
+store.dispatch({type: 'decrease'})
+store.dispatch({type: 'decrease'})
+store.dispatch({type: 'decrease'})
+store.dispatch({type: 'decrease'})
+
+
