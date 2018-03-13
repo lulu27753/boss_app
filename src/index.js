611b546723f1b66fcc754920e3b8bd18e3d086ee
lulu27753 <lulu27753@163.com>
2018-03-13 21:09:48 +0800

    配置chrome—redux 插件

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import App from './App';
 import { counter, add, remove, addAsync } from './index.redux';
 import thunk from 'redux-thunk';
 
-const store = createStore(counter, applyMiddleware(thunk));
+const reduxDevtools = window.devToolsExtension ? window.devToolsExtension() : f => f;
+const store = createStore(counter, compose(
+  applyMiddleware(thunk),
+  reduxDevtools
+));
 function render() {
   ReactDom.render(<App store={store} add={add} remove={remove} addAsync={addAsync}/>, document.getElementById('root'))
 }
