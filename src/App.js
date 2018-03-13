a03da0caf0d203ea23b2748a06064d8f18cd97e4
lulu27753 <lulu27753@163.com>
2018-03-13 22:57:16 +0800

    使用装饰器优化connect代码

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import { connect } from 'react-redux';
 
 import { add, remove, addAsync } from './index.redux';
 
+// // 从store里面提取你需要的数据
+// const mapStatetoProp = (state) => {
+//   return {num: state}
+// }
+// const actionCreators = {
+//   add,
+//   remove,
+//   addAsync
+// };
+// // connect()会把store中的state和action映射到props中
+// App = connect(mapStatetoProp, actionCreators)(App);
+@connect(
+  state => ({num: state}),// 你要state的什么属性放到props里
+  { add, remove, addAsync }// 你要什么方法，放到props里，会自动dispatch
+)
+
 class App extends React.Component {
     render() {
       const num = this.props.num;
@@ -19,11 +35,5 @@ class App extends React.Component {
       )
     }
 }
-// 从store里面提取你需要的数据
-const mapStatetoProp = (state) => {
-  return { num : state }
-}
-const actionCreators = { add, remove, addAsync };
-// connect()会把store中的state和action映射到props中
-App = connect(mapStatetoProp, actionCreators)(App);
+
 export default App;
