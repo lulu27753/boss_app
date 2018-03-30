6156965a4c0694eedf42bebe384777cae3637d2f
lulu27753 <lulu27753@163.com>
2018-03-30 22:23:08 +0800

    没有redux时使用axios

--- src/Auth.js
+++ src/Auth.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
-
+import axios from 'axios'
 
 import { login } from './Auth.redux'
 
@@ -12,10 +12,31 @@ import { login } from './Auth.redux'
 )
 
 export default class Auth extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {}
+    }
+  }
+  
+  componentDidMount = () => {
+    axios.get('/data').then(
+      res => {
+        if (res.status === 200) {
+          this.setState({
+            data: res.data[0]
+          })
+        }
+        console.log(res.data)
+      }
+    )
+  }
+  
   render() {
     const redirectToDashboard = <Redirect to='/dashboard'></Redirect>
     return (
       <div>
+      <h2>我的名字是{this.state.data.user}</h2>
       {this.props.isAuth ? redirectToDashboard : null}
         Auth page
         <button onClick={this.props.login}>登录</button>
