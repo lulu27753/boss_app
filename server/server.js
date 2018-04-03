300ade7ab2a1e05dda1d3a0a3389dd5b9c88ddae
lulu27753 <lulu27753@163.com>
2018-04-03 17:25:00 +0800

    express注册功能实现

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 
 const userRouter = require('./user');
 
 // 新建app
 const app = express()
+
+// 可以接收post请求传过来的数据
+app.use(bodyParser.json())
+
+app.use(cookieParser())
 
 // 只要与“/user”相关的子路由设置为userRouter
 app.use('/user/', userRouter);
--- server/user.js
+++ server/user.js
@@ -2,7 +2,7 @@
 * @Author: lulu27753
 * @Date:   2018-04-02 17:18:56
 * @Last Modified by:   lulu27753
-* @Last Modified time: 2018-04-03 16:26:47
+* @Last Modified time: 2018-04-03 17:22:40
 */
 const express = require('express')
 const models = require('./model.js')
@@ -15,6 +15,28 @@ Router.get('/list', function (req, res) {
 	// 查询用户列表
 	User.find({}, function (err, doc) {
 		return res.json(doc)
+	})
+});
+Router.post('/register', function (req, res) {
+	// 用户注册信息提交
+	console.log(req.body)
+	const { user, pwd, role } = req.body;
+	// 保证每个用户只注册一次
+	User.findOne({user}, function (err, doc) {
+		// 用户已注册
+		if (doc) {
+			return res.json({code: 1, msg: '用户已注册'})
+		} else {
+			// 用户未注册，则新增一条数据
+			User.create({ user, pwd, role }, function (err, doc) {
+				if (err) {
+					res.json({code: 1, msg: '后端出错了！'})
+				} else {
+					// 数据新增成功，则直接授权显示用户已登录，并且将数据存储到cookie中
+					res.json({code: 0})
+				}
+			})
+		}
 	})
 });
 Router.get('/info', function (req, res) {
