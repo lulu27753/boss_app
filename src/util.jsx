0208b5ab254d3212dcf7655de0e35a9765c3eff4
lulu27753 <lulu27753@163.com>
2018-04-06 10:00:46 +0800

    路由跳转bug修复

--- src/util.jsx
+++ src/util.jsx
@@ -1,9 +1,9 @@
-export function getRedirectPath({role, avator}) {
+export function getRedirectPath({role, avatar}) {
 	// 根据用户信息，返回跳转页面地址
 	// user.role 分别跳转到'/custom' | '/service'
 	// user.avator 分别跳转到'/custominfo' | '/serviceinfo'
-	let url;
-	if (!avator) {
+	let url = `/${role}`;
+	if (!avatar) {
 		url = `/${role}info`;
 	}
 	return url;
